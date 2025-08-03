5029b9eddfdc5baf06cc28c15006d80f457d1146
SOUTHEP666 <southep_sichan@hotmail.com>
2025-08-03 12:24:34 +0800

    fix: add

--- src/api/user.js
+++ src/api/user.js
@@ -1,29 +1,29 @@
 import axios from "axios";
 
-const API_URL = "/api/user";
+const BASE_URL = "https://laosecom.onrender.com/api/user";
 
 export function register(data) {
-  return axios.post(`${API_URL}/register`, data);
+  return axios.post(`${BASE_URL}/register`, data);
 }
 
 export function login(data) {
-  return axios.post(`${API_URL}/login`, data);
+  return axios.post(`${BASE_URL}/login`, data);
 }
 
 export function getProfile(token) {
-  return axios.get(`${API_URL}/profile`, {
+  return axios.get(`${BASE_URL}/profile`, {
     headers: { Authorization: `Bearer ${token}` },
   });
 }
 
 export function updateProfile(data, token) {
-  return axios.put(`${API_URL}/profile`, data, {
+  return axios.put(`${BASE_URL}/profile`, data, {
     headers: { Authorization: `Bearer ${token}` },
   });
 }
 
 export function changePassword(data, token) {
-  return axios.post(`${API_URL}/change-password`, data, {
+  return axios.post(`${BASE_URL}/change-password`, data, {
     headers: { Authorization: `Bearer ${token}` },
   });
 }
